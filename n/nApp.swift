04e069bdab30e225544368b6a3923901e0f9f13a
Nada-09 <nadaalmajidi4@gmail.com>
2024-09-29 10:02:30 +0300

    Initial Commit

---
+++ n/nApp.swift
@@ -0,0 +1,17 @@
+//
+//  nApp.swift
+//  n
+//
+//  Created by Nada Abdullah on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct nApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nTests/nTests.swift
@@ -0,0 +1,17 @@
+//
+//  nTests.swift
+//  nTests
+//
+//  Created by Nada Abdullah on 26/03/1446 AH.
+//
+
+import Testing
+@testable import n
+
+struct nTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
